e510f1a893971e84900e650179db7c2d04b3534e
Travis Gorman <thefarbetween@me.com>
2017-03-26 20:52:42 -0500

    searchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
-const SearchBar = React.createClass({
-  render() {
-    return (  
-
-    );
+const Search = React.createClass({
+  render(){
+    return (
+      <form>
+        <input type="text" placeholder="Search..." />
+        <p>
+          <input type="checkbox"  />
+          Show in stock items only
+        </p>
+      </form>
+    )
   }
 })
 
