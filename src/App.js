9b841f91b737610e123c7596ee315b687b3c9462
Travis Gorman <thefarbetween@me.com>
2017-03-26 21:39:18 -0500

    filterableProductTable

--- src/App.js
+++ src/App.js
@@ -4,15 +4,33 @@ import SearchBar from './components/SearchBar'
 import ProductTable from './components/ProductTable'
 
 const App = React.createClass({
+	getInitialState() {
+		return {
+			filterText: '',
+			inStockOnly: false,
+		};
+	},
+	handleUserInput(filterText, inStockOnly) {
+		this.setState({
+			filterText,
+			inStockOnly,
+		});
+	},
   render() {
     return (
       <div>
-        <SearchBar  />
-        <ProductTable   />
+        <Search 
+        	filterText={this.state.filterText} 
+        	inStockOnly={this.state.inStockOnly}
+        	onUserInput={this.handleUserInput} />
+        <Table 
+        	products={this.props.products}
+        	filterText={this.state.filterText}
+        	inStockOnly={this.state.inStockOnly} />
       </div>
-    )
+    );
   }
-})
+});
 
 
 export default App;
