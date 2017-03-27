d5cc9f5ca6d35e2a05b6865d7830ef13141ca2b0
Travis Gorman <thefarbetween@me.com>
2017-03-26 20:55:06 -0500

    filterableTable

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import SearchBar from './components/SearchBar'
 import ProductTable from './components/ProductTable'
 
 const App = React.createClass({
-  
-  render() { 
-    return (  
-      <div className="App">
-        
+  render() {
+    return (
+      <div>
+        <SearchBar  />
+        <ProductTable   />
       </div>
-    );
+    )
   }
-});
+})
 
 
 export default App;
