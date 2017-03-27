73afbc746c7f5ac0f86d1b79700c8555d73c80d1
Travis Gorman <thefarbetween@me.com>
2017-03-26 20:52:00 -0500

    productTable

--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
 const ProductTable = React.createClass({
-  render() {
-    return (  
-
-    );
+  render(){
+    let rows = <tr>I am a row</tr>;
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    )
   }
 })
 
