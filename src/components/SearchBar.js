20dd5059ffd0eb7335a858a9ad5e87c0ed990770
Travis Gorman <thefarbetween@me.com>
2017-03-26 21:37:08 -0500

    searchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,41 @@
 import React from 'react'
 
-const Search = React.createClass({
+const SearchBar = React.createClass({
+  
+  propTypes: {
+    onUserInput: React.PropTypes.func,
+    filterText: React.PropTypes.string,
+    inStockOnly: React.PropTypes.bool,
+  },
+
+  handleChange() {
+    this.props.onUserInput(
+      this.refs.filterTextInput.value,
+      this.refs.inStockOnlyInput.checked,
+    );
+  },
+  
   render(){
     return (
       <form>
-        <input type="text" placeholder="Search..." />
+        <input
+          type="text" 
+          placeholder="Search..."
+          value={this.props.filterText}
+          ref="filterTextInput"
+          onChange={this.handleChange} />    
+          
         <p>
-          <input type="checkbox"  />
-          Show in stock items only
+          <input 
+            type="checkbox" 
+            checked={this.props.inStockOnly}
+            ref="inStockOnlyInput"
+            onChange={this.handleChange} />
+          {' '} Show in stock items only
         </p>
       </form>
     )
   }
-})
+});
 
 export default SearchBar;
